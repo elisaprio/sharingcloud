c4df506f8aeb20515410aa18a0de3e8a2ef6051b
Elisa Priollaud <elisaprio@live.fr>
2019-11-26 22:59:26 +0100

    models in admin

--- mysite/ressource/admin.py
+++ mysite/ressource/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from ressource.models import Ressource, RessourceType, Reservation
+
+admin.site.register(Ressource)
+admin.site.register(RessourceType)
+admin.site.register(Reservation)
